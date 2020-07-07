ec18a5cafb5e225c6bfccc71b17f1b30227d54c0
mjibran <mohdjibran316@gmail.com>
2020-07-07 17:22:52 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace TalkTalk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._BtnStartCam = new System.Windows.Forms.Button();
+            this._BtnStopCam = new System.Windows.Forms.Button();
+            this._pbUser1 = new System.Windows.Forms.PictureBox();
+            this._cmbCamList = new System.Windows.Forms.ComboBox();
+            this._pbUser2 = new System.Windows.Forms.PictureBox();
+            this._btnSend = new System.Windows.Forms.Button();
+            this._txtServer = new System.Windows.Forms.TextBox();
+            this._txtClient = new System.Windows.Forms.TextBox();
+            this._btnConnect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this._pbUser1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._pbUser2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _BtnStartCam
+            // 
+            this._BtnStartCam.Location = new System.Drawing.Point(13, 10);
+            this._BtnStartCam.Name = "_BtnStartCam";
+            this._BtnStartCam.Size = new System.Drawing.Size(110, 23);
+            this._BtnStartCam.TabIndex = 0;
+            this._BtnStartCam.Text = "Start Web Cam";
+            this._BtnStartCam.UseVisualStyleBackColor = true;
+            this._BtnStartCam.Click += new System.EventHandler(this._BtnStartCam_Click);
+            // 
+            // _BtnStopCam
+            // 
+            this._BtnStopCam.Location = new System.Drawing.Point(129, 10);
+            this._BtnStopCam.Name = "_BtnStopCam";
+            this._BtnStopCam.Size = new System.Drawing.Size(110, 23);
+            this._BtnStopCam.TabIndex = 1;
+            this._BtnStopCam.Text = "End Web Cam";
+            this._BtnStopCam.UseVisualStyleBackColor = true;
+            this._BtnStopCam.Click += new System.EventHandler(this._BtnStopCam_Click);
+            // 
+            // _pbUser1
+            // 
+            this._pbUser1.Location = new System.Drawing.Point(30, 119);
+            this._pbUser1.Name = "_pbUser1";
+            this._pbUser1.Size = new System.Drawing.Size(327, 319);
+            this._pbUser1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this._pbUser1.TabIndex = 2;
+            this._pbUser1.TabStop = false;
+            // 
+            // _cmbCamList
+            // 
+            this._cmbCamList.FormattingEnabled = true;
+            this._cmbCamList.Location = new System.Drawing.Point(30, 64);
+            this._cmbCamList.Name = "_cmbCamList";
+            this._cmbCamList.Size = new System.Drawing.Size(271, 24);
+            this._cmbCamList.TabIndex = 3;
+            // 
+            // _pbUser2
+            // 
+            this._pbUser2.Location = new System.Drawing.Point(425, 119);
+            this._pbUser2.Name = "_pbUser2";
+            this._pbUser2.Size = new System.Drawing.Size(327, 319);
+            this._pbUser2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this._pbUser2.TabIndex = 4;
+            this._pbUser2.TabStop = false;
+            // 
+            // _btnSend
+            // 
+            this._btnSend.Location = new System.Drawing.Point(259, 13);
+            this._btnSend.Name = "_btnSend";
+            this._btnSend.Size = new System.Drawing.Size(75, 23);
+            this._btnSend.TabIndex = 5;
+            this._btnSend.Text = "Send";
+            this._btnSend.UseVisualStyleBackColor = true;
+            this._btnSend.Click += new System.EventHandler(this._btnSend_Click);
+            // 
+            // _txtServer
+            // 
+            this._txtServer.AcceptsReturn = true;
+            this._txtServer.Location = new System.Drawing.Point(786, 10);
+            this._txtServer.Multiline = true;
+            this._txtServer.Name = "_txtServer";
+            this._txtServer.Size = new System.Drawing.Size(204, 186);
+            this._txtServer.TabIndex = 6;
+            // 
+            // _txtClient
+            // 
+            this._txtClient.AcceptsReturn = true;
+            this._txtClient.Location = new System.Drawing.Point(786, 329);
+            this._txtClient.Multiline = true;
+            this._txtClient.Name = "_txtClient";
+            this._txtClient.Size = new System.Drawing.Size(204, 186);
+            this._txtClient.TabIndex = 7;
+            // 
+            // _btnConnect
+            // 
+            this._btnConnect.Location = new System.Drawing.Point(358, 12);
+            this._btnConnect.Name = "_btnConnect";
+            this._btnConnect.Size = new System.Drawing.Size(75, 23);
+            this._btnConnect.TabIndex = 8;
+            this._btnConnect.Text = "Connect";
+            this._btnConnect.UseVisualStyleBackColor = true;
+            this._btnConnect.Click += new System.EventHandler(this._btnConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1017, 527);
+            this.Controls.Add(this._btnConnect);
+            this.Controls.Add(this._txtClient);
+            this.Controls.Add(this._txtServer);
+            this.Controls.Add(this._btnSend);
+            this.Controls.Add(this._pbUser2);
+            this.Controls.Add(this._cmbCamList);
+            this.Controls.Add(this._pbUser1);
+            this.Controls.Add(this._BtnStopCam);
+            this.Controls.Add(this._BtnStartCam);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this._pbUser1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._pbUser2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _BtnStartCam;
+        private System.Windows.Forms.Button _BtnStopCam;
+        private System.Windows.Forms.PictureBox _pbUser1;
+        private System.Windows.Forms.ComboBox _cmbCamList;
+        private System.Windows.Forms.PictureBox _pbUser2;
+        private System.Windows.Forms.Button _btnSend;
+        private System.Windows.Forms.TextBox _txtServer;
+        private System.Windows.Forms.TextBox _txtClient;
+        private System.Windows.Forms.Button _btnConnect;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TalkTalk
+{
+    public partial class Form1 : Form
+    {
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+        int portToConnect = 61000;
+        public Form1()
+        {
+            InitializeComponent();
+            StartServer();
+        }
+
+        #region  WebCam
+        private void _BtnStartCam_Click(object sender, EventArgs e)
+        {
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[_cmbCamList.SelectedIndex].MonikerString);
+            videoCaptureDevice.NewFrame += videoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+
+        }
+
+        private void videoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            _pbUser1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in filterInfoCollection)
+            {
+                _cmbCamList.Items.Add(device.Name);
+            }
+
+            _cmbCamList.SelectedIndex = 0;
+
+        }
+
+        private void _BtnStopCam_Click(object sender, EventArgs e)
+        {
+            ReleaseResources();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ReleaseResources();
+        }
+
+        private void ReleaseResources()
+        {
+            if (videoCaptureDevice.IsRunning)
+                videoCaptureDevice.Stop();
+        }
+
+        #endregion
+
+
+        #region SocketsServer
+        private Socket _serverSocketServer, _clientSocketServer;
+        private byte[] _buffer;
+
+        private void ShowExceptionBox(string msg)
+        {
+            MessageBox.Show(msg, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void StartServer()
+        {
+            try
+            {
+                _serverSocketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                _serverSocketServer.Bind(new IPEndPoint(IPAddress.Any, portToConnect));
+                _serverSocketServer.Listen(0);
+                _serverSocketServer.BeginAccept(new AsyncCallback(AcceptCallBack), null);
+            }
+            catch (Exception ex)
+            {
+                ShowExceptionBox(ex.Message);
+            }
+        }
+
+        private void AcceptCallBack(IAsyncResult AR)
+        {
+            try
+            {
+                _clientSocketServer = _serverSocketServer.EndAccept(AR);
+                _buffer = new byte[_clientSocketServer.ReceiveBufferSize];
+                _clientSocketServer.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallBack), null);
+            }
+            catch (Exception ex)
+            {
+                ShowExceptionBox(ex.Message);
+            }
+        }
+
+        private void ReceiveCallBack(IAsyncResult AR)
+        {
+            try
+            {
+                int received = _clientSocketServer.EndReceive(AR);
+                Array.Resize(ref _buffer, received);
+                string text = Encoding.ASCII.GetString(_buffer);
+                AppendToTextBox(text);
+                Array.Resize(ref _buffer, _clientSocketServer.ReceiveBufferSize);
+                _clientSocketServer.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallBack), null);
+            }
+            catch (Exception ex)
+            {
+                ShowExceptionBox(ex.Message);
+            }
+        }
+
+        private void AppendToTextBox(string text)
+        {
+            MethodInvoker invoker = new MethodInvoker(delegate
+            {
+                _txtServer.Text += text + "/n/r";
+            }
+                );
+
+            this.Invoke(invoker);
+        }
+        #endregion
+
+        #region SocketsClient
+        private Socket _clientSocketClient;
+        private byte[] buffer;
+
+        private void _btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _clientSocketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                _clientSocketClient.BeginConnect(new IPEndPoint(IPAddress.Loopback, portToConnect), new AsyncCallback(ConnectCallBack), null);
+            }
+            catch (Exception ex)
+            {
+                ShowExceptionBox(ex.Message);
+            }
+        }
+
+        private void ConnectCallBack(IAsyncResult AR)
+        {
+            try
+            {
+                _clientSocketClient.EndConnect(AR);
+
+                MessageBox.Show("Connected", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                ShowExceptionBox(ex.Message);
+            }
+        }
+
+        private void _btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(_txtClient.Text);
+                _clientSocketClient.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(SendCallBack), null);
+            }
+            catch (Exception ex)
+            {
+                ShowExceptionBox(ex.Message);
+            }
+        }
+
+        private void SendCallBack(IAsyncResult AR)
+        {
+            try
+            {
+                _clientSocketClient.EndSend(AR);
+            }
+            catch (Exception ex)
+            {
+                ShowExceptionBox(ex.Message);
+            }
+        }
+        #endregion
+
+    }
+}
